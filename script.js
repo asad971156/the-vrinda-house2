5c4a39b01f9136093d26e19506c5c6fd93bc0fcb
asad971156 <recreationservices.rs@gmail.com>
2026-07-17 01:59:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -233,3 +233,13 @@ menuToggle.addEventListener("click", () => {
     nav.classList.toggle("active");
 
 });
+
+document.querySelectorAll("nav a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        nav.classList.remove("active");
+
+    });
+
+});
