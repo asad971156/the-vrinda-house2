2f20c759587df4ab6a13446c8ddb2abe362e496e
asad971156 <recreationservices.rs@gmail.com>
2026-07-17 01:52:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -201,3 +201,21 @@ navLinks.forEach(link=>{
     });
 
 });
+
+// ==========================
+// Scroll Progress Bar
+// ==========================
+
+window.addEventListener("scroll", () => {
+
+    const scrollTop = document.documentElement.scrollTop;
+
+    const scrollHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+
+    const progress = (scrollTop / scrollHeight) * 100;
+
+    document.getElementById("progress-bar").style.width = progress + "%";
+
+});
